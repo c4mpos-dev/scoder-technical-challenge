1f50d3340150d91fb1828290d48e9a2da6cb8453
Cauã Campos <campos.eet@gmail.com>
2025-06-07 20:59:23 -0300

    :tada: Initial presets

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+export function App() {
+    return (
+        <div className='flex justify-center items-center h-screen bg-purple-400'>
+            <h1 className='text-white text-5xl'>FAKE STORE</h1>
+        </div>
+    )
+}
